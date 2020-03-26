a35f8228575635e43d2d999fa372cd9d626eb05c
kent <silnith@gmail.com>
2020-03-26 11:15:46 -0700

    Fix menu factory to create a new menu for each frame.

--- text-layout/src/main/java/org/silnith/text/layout/LookAndFeelMenuFactory.java
+++ text-layout/src/main/java/org/silnith/text/layout/LookAndFeelMenuFactory.java
@@ -1,28 +1,36 @@
 package org.silnith.text.layout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
 public class LookAndFeelMenuFactory {
 
-	private final JMenu lookAndFeelMenu;
+	private final List<Action> actions;
 
 	public LookAndFeelMenuFactory() {
-		lookAndFeelMenu = new JMenu("Look and Feel");
-		for (final LookAndFeelInfo lookAndFeelInfo : UIManager.getInstalledLookAndFeels()) {
+		final LookAndFeelInfo[] installedLookAndFeels = UIManager.getInstalledLookAndFeels();
+		actions = new ArrayList<Action>(installedLookAndFeels.length);
+		for (final LookAndFeelInfo lookAndFeelInfo : installedLookAndFeels) {
 			final String name = lookAndFeelInfo.getName();
 			final String className = lookAndFeelInfo.getClassName();
-		
+
 			final SetLookAndFeelAction setLookAndFeelAction = new SetLookAndFeelAction(name, className);
 			// TODO: Set any properties on the action.
-		
-			lookAndFeelMenu.add(setLookAndFeelAction);
+			actions.add(setLookAndFeelAction);
 		}
 	}
 
 	public JMenu getLookAndFeelMenu() {
+		final JMenu lookAndFeelMenu = new JMenu("Look and Feel");
+		for (final Action action : actions) {
+			lookAndFeelMenu.add(action);
+		}
 		return lookAndFeelMenu;
 	}
-	
+
 }
